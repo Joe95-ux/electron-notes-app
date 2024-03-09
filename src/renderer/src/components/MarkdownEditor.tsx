072794d7f390dc17b5ef2fa332783107d4bb2a74
joe95-ux <ogorktabi@gmail.com>
2024-03-09 23:25:00 +0200

    add codeblock support

--- src/renderer/src/components/MarkdownEditor.tsx
+++ src/renderer/src/components/MarkdownEditor.tsx
@@ -1,11 +1,19 @@
 import {
+  ChangeCodeMirrorLanguage,
+  ConditionalContents,
+  InsertCodeBlock,
+  InsertSandpack,
   MDXEditor,
+  SandpackConfig,
+  ShowSandpackInfo,
   codeBlockPlugin,
   codeMirrorPlugin,
   headingsPlugin,
   listsPlugin,
   markdownShortcutPlugin,
-  quotePlugin
+  quotePlugin,
+  sandpackPlugin,
+  toolbarPlugin
 } from '@mdxeditor/editor'
 import { useMarkdownEditor } from '@renderer/hooks/useMarkdownEditor'
 
@@ -25,6 +33,22 @@ export const MarkdownEditor = () => {
   }
   `.trim()
 
+  const simpleSandpackConfig: SandpackConfig = {
+    defaultPreset: 'react',
+    presets: [
+      {
+        label: 'React',
+        name: 'react',
+        meta: 'live react',
+        sandpackTemplate: 'react',
+        sandpackTheme: 'light',
+        snippetFileName: '/App.js',
+        snippetLanguage: 'jsx',
+        initialSnippetContent: defaultSnippetContent
+      }
+    ]
+  }
+
   return (
     <MDXEditor
       ref={editorRef}
@@ -38,7 +62,32 @@ export const MarkdownEditor = () => {
         quotePlugin(),
         markdownShortcutPlugin(),
         codeBlockPlugin({ defaultCodeBlockLanguage: 'js' }),
-        codeMirrorPlugin({ codeBlockLanguages: { js: 'JavaScript', css: 'CSS' } })
+        sandpackPlugin({ sandpackConfig: simpleSandpackConfig }),
+        codeMirrorPlugin({ codeBlockLanguages: { js: 'JavaScript', css: 'CSS' } }),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <ConditionalContents
+              options={[
+                {
+                  when: (editor) => editor?.editorType === 'codeblock',
+                  contents: () => <ChangeCodeMirrorLanguage />
+                },
+                {
+                  when: (editor) => editor?.editorType === 'sandpack',
+                  contents: () => <ShowSandpackInfo />
+                },
+                {
+                  fallback: () => (
+                    <>
+                      <InsertCodeBlock />
+                      <InsertSandpack />
+                    </>
+                  )
+                }
+              ]}
+            />
+          )
+        })
       ]}
       contentEditableClassName="outline-none min-h-screen max-w-none text-lg px-8 py-5 caret-yellow-500 prose prose-invert prose-p:my-3 prose-p:leading-relaxed prose-headings:my-4 prose-blockquote:my-4 prose-ul:my-2 prose-li:my-0 prose-code:px-1 prose-code:text-red-500 prose-code:before:content-[''] prose-code:after:content-['']"
     />
